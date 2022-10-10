5ba5787d2c1907e10d363251c782d816f9bf1be3
Takafumi Umemoto <takafumi.umemoto@gmail.com>
2022-10-10 01:56:02 +0000

    Initial Commit

---
+++ index.py
@@ -0,0 +1,14 @@
+# %%
+from rembg import remove
+
+# %%
+input_path = './src/input.png'
+output_path = './dst/output.png'
+
+# %%
+with open(input_path, 'rb') as i:
+    with open(output_path, 'wb') as o:
+        input = i.read()
+        output = remove(input)
+        o.write(output)
+# %%
